2d02638fcf5ef5edd0f92d5ee104b71b77452a11
Nothize <nothize@gmail.com>
2018-12-10 03:26:01 +0800

    Add the edges

--- draw.go
+++ draw.go
@@ -5,15 +5,18 @@ import (
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/imdraw"
 	"golang.org/x/image/colornames"
+	"time"
+	"github.com/sirupsen/logrus"
 )
 
 type D struct {
-	Size   int
-	Cfg    *pixelgl.WindowConfig
-	Imd    *imdraw.IMDraw
-	Win    *pixelgl.Window
-	Pad    float64
-	Offset pixel.Vec
+	GridConfig *GridConfig
+	Size       int
+	Cfg        *pixelgl.WindowConfig
+	Imd        *imdraw.IMDraw
+	Win        *pixelgl.Window
+	Pad        float64
+	Offset     pixel.Vec
 }
 
 func (d *D) run() {
@@ -22,20 +25,21 @@ func (d *D) run() {
 	imd := d.Imd
 
 	d.DrawBoard()
+	t := time.NewTicker(time.Second / 30)
 	for !d.Win.Closed() {
 		imd.Draw(d.Win)
 		d.Win.Update()
+		<- t.C
 	}
 }
 
-func NewDraw() *D {
-	d := &D{}
+func NewDraw(gc *GridConfig) *D {
+	d := &D{GridConfig: gc, Size: gc.Size}
 	return d
 }
 
 func (d *D) Init() {
 	var err error
-	d.Size = 7
 	d.Pad = 40.0
 	d.Cfg = &pixelgl.WindowConfig{
 		Title: "Smallest sum of products - Grid",
@@ -51,7 +55,6 @@ func (d *D) Init() {
 
 	d.Win, err = pixelgl.NewWindow(*d.Cfg)
 	d.Win.Clear(colornames.Aliceblue)
-
 
 	if err != nil {
 		panic(err)
@@ -70,5 +73,35 @@ func (d *D) DrawBoard(reset ...bool) {
 			d.Imd.Push(xy, xy.Add(xypad))
 		}
 		d.Imd.Rectangle(1)
+	}
+	xy := pixel.V(float64(0), float64(0)).Scaled(d.Pad).Add(d.Offset)
+	xy2 := pixel.V(float64(d.Size), float64(d.Size)).Scaled(d.Pad).Add(d.Offset)
+	d.Imd.Push(xy, xy2)
+	d.Imd.Rectangle(4)
+
+	h := d.GridConfig.GetHorizontal()
+	for i := 0; i < len(h); i++ {
+		for j := 0; j < len(h[i]); j++ {
+			if h[i][j] {
+				x := j
+				y := d.GridConfig.Size - (i + 1)
+				xy := pixel.V(float64(x), float64(y)).Scaled(d.Pad).Add(d.Offset)
+				d.Imd.Push(xy, xy.Add(pixel.Vec{ d.Pad, 0}))
+				d.Imd.Line(4)
+			}
+		}
+	}
+	v := d.GridConfig.GetVerticle()
+	for i := 0; i < len(v); i++ {
+		for j := 0; j < len(v[i]); j++ {
+			if v[i][j] {
+				logrus.Infof("%d, %d = %b", i, j, v[i][j])
+				x := i + 1
+				y := d.GridConfig.Size - (j + 1)
+				xy := pixel.V(float64(x), float64(y)).Scaled(d.Pad).Add(d.Offset)
+				d.Imd.Push(xy, xy.Add(pixel.Vec{ 0, d.Pad}))
+				d.Imd.Line(4)
+			}
+		}
 	}
 }
--- main.go
+++ main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"github.com/faiface/pixel/pixelgl"
+	"io/ioutil"
+	"log"
 )
 
 func main() {
-	d := NewDraw()
+	cfg, err := ioutil.ReadFile("gridconfig.yaml")
+	if err != nil {
+		log.Fatal("Config file not found")
+	}
+	d := NewDraw(NewGridConfig(string(cfg)))
 
 	pixelgl.Run(d.run)
 }
